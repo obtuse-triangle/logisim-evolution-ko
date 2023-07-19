04a03dd164598a54499b5bee3e1ada1f2433428a
Kevin Walsh <kwalsh@holycross.edu>
2023-07-19 01:03:11 -0400

    MidiIn fixes and ifaces
    Other interfaces for midi input.
    Midi input bug fixes.

--- src/com/cburch/logisim/std/audio/MidiIn.java
+++ src/com/cburch/logisim/std/audio/MidiIn.java
@@ -34,18 +34,20 @@ import static com.cburch.logisim.std.Strings.S;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Receiver;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Synthesizer;
 import javax.sound.midi.Transmitter;
 
 import com.cburch.logisim.data.Attribute;
 import com.cburch.logisim.data.AttributeOption;
-import com.cburch.logisim.data.AttributeSet;
-import com.cburch.logisim.data.Attributes;
 import com.cburch.logisim.data.BitWidth;
 import com.cburch.logisim.data.Bounds;
 import com.cburch.logisim.data.Direction;
@@ -57,6 +59,8 @@ import com.cburch.logisim.instance.InstancePainter;
 import com.cburch.logisim.instance.InstanceState;
 import com.cburch.logisim.instance.Port;
 import com.cburch.logisim.instance.StdAttr;
+import com.cburch.logisim.util.Debug;
+import com.cburch.logisim.util.GraphicsUtil;
 
 public class MidiIn extends InstanceFactory {
 
@@ -83,17 +87,9 @@ public class MidiIn extends InstanceFactory {
     super("MidiIn", S.getter("audioMidiInComponent"));
     setIconName("midiin.gif");
     setOffsetBounds(Bounds.create(-30, -30, 30, 60));
+    setAttributes(new Attribute[] { StdAttr.EDGE_TRIGGER, ATTR_IFACE},
+        new Object[] { StdAttr.TRIG_RISING, IFACE_SERIAL });
     setPorts(makePorts(IFACE_SERIAL));
-  }
-
-  @Override
-  public AttributeSet createAttributeSet() {
-    // We defer init to here, so that Midi is only initialized when being used
-    if (init()) {
-      setAttributes(new Attribute[] { StdAttr.EDGE_TRIGGER, ATTR_IFACE},
-          new Object[] { StdAttr.TRIG_RISING, IFACE_SERIAL });
-    }
-    return super.createAttributeSet();
   }
 
   @Override
@@ -141,13 +137,13 @@ public class MidiIn extends InstanceFactory {
         ps[OUT] = new Port(0, 0, Port.OUTPUT, BitWidth.of(8));
         ps[OUT].setToolTip(S.getter("midiOutputSerial"));
       } else if (iface == IFACE_PARALLEL3) {
-        ps[OUT] = new Port(-30, 0, Port.OUTPUT, BitWidth.of(24));
+        ps[OUT] = new Port(0, 0, Port.OUTPUT, BitWidth.of(24));
         ps[OUT].setToolTip(S.getter("midiOutputParallel3"));
       } else if (iface == IFACE_PARALLEL4) {
-        ps[OUT] = new Port(-30, 0, Port.OUTPUT, BitWidth.of(32));
+        ps[OUT] = new Port(0, 0, Port.OUTPUT, BitWidth.of(32));
         ps[OUT].setToolTip(S.getter("midiOutputParallel4"));
       } else { /* IFACE_LOGISIM1 */
-        ps[OUT] = new Port(-30, 0, Port.OUTPUT, BitWidth.of(32));
+        ps[OUT] = new Port(0, 0, Port.OUTPUT, BitWidth.of(32));
         ps[OUT].setToolTip(S.getter("midiOutputLogisim1"));
       }
     }
@@ -156,8 +152,7 @@ public class MidiIn extends InstanceFactory {
 
   @Override
   public void propagate(InstanceState state) {
-    if (midiFailed)
-      return;
+    enumerate();
 
     Object trigger = state.getAttributeValue(StdAttr.EDGE_TRIGGER);
     State data = getState(state);
@@ -177,6 +172,7 @@ public class MidiIn extends InstanceFactory {
 
     // check if new data is available, either 1 byte (serial) or 1-3 bytes (others)
     if (iface == IFACE_SERIAL) {
+
       boolean avail = false;
       int b0 = -1;
       synchronized (midiLock) {
@@ -188,9 +184,142 @@ public class MidiIn extends InstanceFactory {
       state.setPort(RDY, avail ? Value.TRUE : Value.FALSE, 1);
       if (avail)
         state.setPort(OUT, Value.createKnown(BitWidth.of(8), b0), 1);
+      return;
+
     } else {
-      // todo
-    }
+
+      int max;
+      if (iface == IFACE_PARALLEL3) max = 3;
+      else if (iface == IFACE_PARALLEL4) max = 4;
+      else max = 3; // IFACE_LOGISIM1 || IFACE_LOGISIM5
+
+      int b0 = 0, b1 = 0, b2 = 0, b3 = 0;
+      boolean avail = false, discard = true;
+      while (discard) { /* examine queue */
+        discard = false;
+        avail = false;
+        b0 = b1 = b2 = b3 = 0;
+        synchronized (midiLock) {
+          // remove stray partial messages
+          while (queue.n > 0 && (queue.peek(0) & 0x80) == 0)
+            queue.remove();
+          // check for empty queue
+          if (queue.n == 0)
+            break; /* stop examine queue */
+          // check MIDI status byte at head of queue 
+          b0 = queue.peek(0);
+          int expecting;
+          if (((b0 >> 4) & 0xF) == 0x9) // MIDI NoteOn
+            expecting = 2;
+          else if (((b0 >> 4) & 0xF) == 0x8) // MIDI NoteOff
+            expecting = 2;
+          else if (((b0 >> 4) & 0xF) == 0xC) // MIDI Program Change
+            expecting = 1;
+          else // MIDI Reset or unrecognized
+            expecting = 0;
+          // check if enough parameter bytes
+          if (expecting > 0) {
+            avail = (queue.n > expecting);
+          } else {
+            avail = true;
+            // take as many parameter bytes as can be found, up to max
+            while (1+expecting < max &&
+                1+expecting < queue.n &&
+                (queue.peek(1+expecting) & 0x80) == 0)
+              expecting++;
+          }
+          // check if success so far
+          if (!avail)
+            break; /* stop examine queue */
+          // grab parameter bytes, discard message if incomplete
+          if (expecting >= 1) {
+            b1 = queue.peek(1);
+            if ((b1 & 0x80) != 0) {
+              System.out.println("MIDI: first param byte missing");
+              queue.remove();
+              continue; /* repeat examine queue */
+            }
+          }
+          if (expecting >= 2) {
+            b2 = queue.peek(2);
+            if ((b2 & 0x80) != 0) {
+              System.out.println("MIDI: second param byte missing");
+              queue.remove();
+              queue.remove();
+              continue; /* repeat examine queue */
+            }
+          }
+          if (expecting >= 3) {
+            b3 = queue.peek(3);
+            if ((b3 & 0x80) != 0) {
+              System.out.println("MIDI: third param byte missing");
+              queue.remove();
+              queue.remove();
+              queue.remove();
+              continue; /* repeat examine queue */
+            }
+          }
+          /* filter unusable messages if needed */
+          if (iface == IFACE_LOGISIM1 || iface == IFACE_LOGISIM5) {
+            if (((b0 >> 4) & 0xF) == 0x9) { // MIDI NoteOn
+              // hurray
+            } else if (((b0 >> 4) & 0xF) == 0x8) { // MIDI NoteOff
+              // will handle below
+            } else if (((b0 >> 4) & 0xF) == 0xC) { // MIDI Program Change
+              int chan = b0 & 0xF;
+              midiChannelInstrument[chan] = b1;
+              discard = true;
+            } else if (b0 == 0xff) { // MIDI Reset
+              for (int chan = 0; chan < 16; chan++)
+                midiChannelInstrument[chan] = 0;
+              discard = true;
+            } else { // Unrecognized
+              discard = true;
+            }
+          }
+          // advance queue if needed
+          if (advance || discard) {
+            for (int i = 0; i < 1 + expecting; i++)
+              queue.remove();
+          }
+        } // synchronized
+      } /* done examine queue */
+
+      if (!avail) {
+        state.setPort(RDY, Value.FALSE, 1);
+        return;
+      }
+
+      state.setPort(RDY, Value.TRUE, 1);
+      if (iface == IFACE_PARALLEL3) {
+        state.setPort(OUT, Value.createKnown(BitWidth.of(24),
+              ((b2&0xff) << 16) | ((b1&0xff) << 8) | (b0&0xff)), 1);
+        return;
+      } else if (iface == IFACE_PARALLEL4) {
+        state.setPort(OUT, Value.createKnown(BitWidth.of(32),
+              ((b3&0xff) << 24) | ((b2&0xff) << 16) | ((b1&0xff) << 8) | (b0&0xff)), 1);
+        return;
+      } else { // IFACE_LOGISIM1 || IFACE_LOGISIM5
+        int stat = (b0 >> 4) & 0xF;
+        int chan = b0 & 0xF;
+        int note = (stat == 0x8) ? (256 - (b1&0x7f)) : (b1&0x7f);
+        int velo = (b2&0x7f);
+        int inst = midiChannelInstrument[chan] & 0xff;
+
+        if (iface == IFACE_LOGISIM1) {
+          Value v = Value.createKnown(BitWidth.of(32),
+              (chan << 28) | (inst << 16) | (velo << 8) | note);
+          state.setPort(OUT, v, 1);
+        } else { /* IFACE_LOGISIM5 */
+          state.setPort(NOTE, Value.createKnown(BitWidth.EIGHT, note), 1);
+          state.setPort(VELO, Value.createKnown(BitWidth.SEVEN, velo), 1);
+          state.setPort(DAMP, Value.FALSE, 1);
+          state.setPort(CHAN, Value.createKnown(BitWidth.FOUR, chan), 1);
+          state.setPort(INST, Value.createKnown(BitWidth.EIGHT, inst), 1);
+        }
+      }
+
+    } /* which iface */
 
   }
 
@@ -214,17 +343,19 @@ public class MidiIn extends InstanceFactory {
     int y = bds.y + (bds.height-24)/2 - 10;
     g.setColor(Color.BLACK);
     g.drawOval(x, y, 24, 24);
-    g.drawOval(x+2, y+11, 4, 4);
-    g.drawOval(x+19, y+11, 4, 4);
-    g.drawOval(x+4, y+5, 4, 4);
-    g.drawOval(x+17, y+5, 4, 4);
-    g.drawOval(x+12, y+2, 4, 4);
-    g.drawArc(x+9, y+21, 6, 7, 10, 160);
+    g.drawOval(x+2, y+10, 4, 4);
+    g.drawOval(x+18, y+10, 4, 4);
+    g.drawOval(x+4, y+4, 4, 4);
+    g.drawOval(x+16, y+4, 4, 4);
+    g.drawOval(x+10, y+2, 4, 4);
+    g.drawArc(x+9, y+20, 6, 8, 10, 160);
 
-    if (!midiFailed)
+    int n = numDevices;
+    if (n > 0)
       g.setColor(Color.BLUE);
     else
       g.setColor(Color.RED);
+    GraphicsUtil.drawText(g, ""+n, x+3, y+40, GraphicsUtil.H_CENTER, GraphicsUtil.V_CENTER);
     g.drawLine(x+12, y+27, x+12, y+36);
     g.drawLine(x+12, y+36, x+24, y+36);
     g.drawLine(x+24, y+36, x+19, y+31);
@@ -233,67 +364,84 @@ public class MidiIn extends InstanceFactory {
 
   // Global state, since the underlying midi system is global anyway
   static Object midiLock = new Object();
-  static boolean midiFailed = false;
+  static int numDevices = 0;
+  static long lastEnumeration = 0;
   
   // static Synthesizer midiSynth = null;
   // static Releaser midiReleaser;
   // static Instrument[] midiInstrument;
   // static MidiChannel[] midiChannel;
   // static int numChannels;
-  // static int[] midiChannelInstrument;
+  static int[] midiChannelInstrument = new int[] {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-  private static boolean init() {
+
+  private static void enumerate() {
+    long now = System.currentTimeMillis();
     synchronized(midiLock) {
-      if (midiFailed || queue != null)
-        return false;
+      if (now - lastEnumeration < 5000)
+        return;
+      lastEnumeration = now;
+        
+      Debug.printf(1, "Enumerating MIDI devices...\n");
+      HashSet<MidiDevice> found = new HashSet<>();
       try {
         MidiDevice.Info[] info = MidiSystem.getMidiDeviceInfo();
-        if (info == null || info.length == 0)
-          throw new MidiUnavailableException("No midi devices available");
-
-        System.out.printf("%d MIDI devices found\n", info.length);
-        for (int i = info.length - 1; i >= 0; i--) {
-          System.out.printf("Querying MIDI device %d: %s\n", i, info[i]);
+        Debug.printf(1, "%d MIDI devices found\n", info.length);
+        for (int i = 0; i < info.length; i++) {
+          Debug.printf(1, "Querying MIDI device %d: %s\n", i, info[i]);
           MidiDevice dev = MidiSystem.getMidiDevice(info[i]);
           if (dev.getMaxTransmitters() == 0)
             continue;
+          if ((dev instanceof Sequencer) || (dev instanceof Synthesizer))
+            continue;
 
+          Rx r = openedDevices.get(dev);
+          if (r != null) {
+            Debug.printf(1, " * already opened, appears to be working normally\n");
+            found.add(dev);
+            continue;
+          }
           try {
             dev.open();
+            Debug.printf(1, " * opened device\n");
           } catch (Exception e) {
-            System.out.printf(" * device not available: %s\n", e.getMessage());
+            Debug.printf(1, " * device not available: %s\n", e.getMessage());
             continue;
           }
-          openedDevices.add(dev);
-          System.out.printf(" * opened device\n");
-
-          if (queue == null)
-            queue = new Queue();
           try {
             Transmitter t = dev.getTransmitter();
-            t.setReceiver(new Rx(dev, t));
+            r = new Rx(dev, t);
+            t.setReceiver(r);
           } catch (Exception e) {
-            try {
-              dev.close();
-              openedDevices.remove(dev);
-            } catch (Exception e2) { }
-            System.out.printf(" * device failed: %s\n", e.getMessage());
+            try { dev.close(); }
+            catch (Exception e2) { }
+            Debug.printf(1, " * device failed: %s\n", e.getMessage());
             continue;
           }
-        }
-       
-        if (openedDevices.isEmpty()) {
-          queue = null;
-          midiFailed = true;
-          System.out.println("No MIDI input devices available");
-        } else {
-          midiFailed = false;
-          System.out.println("MIDI input from " + openedDevices.size() + " devices");
+
+          openedDevices.put(dev, r);
+          found.add(dev);
         }
       } catch (MidiUnavailableException e) {
-        midiFailed = true;
+        e.printStackTrace();
       }
-      return true;
+
+      ArrayList<MidiDevice> toClose = new ArrayList<>();
+      for (MidiDevice dev : openedDevices.keySet()) {
+        if (!found.contains(dev))
+          toClose.add(dev);
+      }
+      if (!toClose.isEmpty()) {
+        Debug.printf(1, "Closing %d stale MIDI devices...\n", toClose.size());
+        for (MidiDevice dev: toClose) {
+          Rx r = openedDevices.get(dev);
+          r.close();
+        }
+      }
+
+      numDevices = openedDevices.size();
+      Debug.printf(1, "MIDI input from %d devices\n", numDevices);
     }
   }
 
@@ -309,8 +457,7 @@ public class MidiIn extends InstanceFactory {
   }
 
   // A single, global queue for all MidiIn components, all devices, etc.
-  static Queue queue;
-  static ArrayList<MidiDevice> openedDevices = new ArrayList<>();
+  static Queue queue = new Queue();
 
   static class Queue {
     byte[] queue = new byte[1000];
@@ -342,6 +489,8 @@ public class MidiIn extends InstanceFactory {
     }
   }
 
+  static HashMap<MidiDevice, Rx> openedDevices = new HashMap<>();
+
   static class Rx implements Receiver {
     MidiDevice dev;
     Transmitter tx;
@@ -352,19 +501,23 @@ public class MidiIn extends InstanceFactory {
     }
 
     public void close() {
+      Debug.println(1, " * Closed MIDI device: " + dev);
       synchronized(midiLock) {
         try { dev.close(); }
         catch (Exception e) { }
         openedDevices.remove(dev);
+        numDevices--;
       }
     }
 
     public void send(MidiMessage message, long timeStamp) {
       byte[] msg = message.getMessage();
-      String s = String.format("%d: (%d bytes)", timeStamp, msg.length);
-      for (int i = 0; i < msg.length; i++)
-        s += String.format(" %02x", msg[i]);
-      System.out.println(s);
+      if (Debug.verbose >= 1) {
+        String s = String.format("%d: (%d bytes)", timeStamp, msg.length);
+        for (int i = 0; i < msg.length; i++)
+          s += String.format(" %02x", msg[i]);
+        System.out.println(s);
+      }
       synchronized(midiLock) {
         queue.add(msg);
       }
